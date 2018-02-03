af437a13c25b481d6d236e1afb191db8d4d8fa93
Sean Carolan <scarolan@hashicorp.com>
2018-02-02 21:49:25 -0600

    Update comment.

--- infrastructure-as-code/aws-lambda-ec2-lifecycles/variables.tf
+++ infrastructure-as-code/aws-lambda-ec2-lifecycles/variables.tf
@@ -5,7 +5,6 @@ variable "region" {
 
 # Unfortunately we don't have a way to auto-encrypt this (yet).  
 # See here for details: https://github.com/hashicorp/terraform/issues/12225
-# So we have to do one manual step to encrypt the URL in the GUI.
 variable "slack_hook_url" {
   default = "https://hooks.slack.com/services/REPLACE/WITH/YOUR/WEBHOOK"
   description = "Slack incoming webhook URL, get this from the slack management page."
