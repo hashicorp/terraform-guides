34bc2badf85769592f0227c8c22bd741d9e253a4
Sean Carolan <scarolan@hashicorp.com>
2018-02-08 15:12:04 -0600

    Removed unnecessary functions from notifySlackUntaggedInstances.py lambda. Created new Lambda for checking TTLs.

--- infrastructure-as-code/aws-lambda-ec2-lifecycles/files/checkInstanceTTLs.py
+++ infrastructure-as-code/aws-lambda-ec2-lifecycles/files/checkInstanceTTLs.py
@@ -27,30 +27,18 @@ lam = boto3.client('lambda')
 def lambda_handler(event, context):
     """Sends out a formatted slack message.  Edit to your liking."""
     
-    msg_text = 'Hello humans. Some of you have not tagged your AWS instances yet.'
-    leaderboard_length = 15
-    untagged = get_untagged_instances()
-    lb = generate_leaderboard(untagged, leaderboard_length)
+    # msg_text = 'The Reaper Cometh :reaper:'
+    # expired = generate_expired_dict(tagged)
+    tagged = get_tagged_instances()
+    return tagged
     
-    send_slack_message(
-        msg_text, 
-        title='The Wall Of Shame :shame: :bell:',
-        text="```\n"+lb+"\n```",
-        fallback='The Wall of Shame :shame: :bell:',
-        color='warning',
-        actions = [
-            {
-                "type": "button",
-                "text": ":mag: Find my stuff",
-                "url": "https://hashicorp.slack.com/files/U8QGF2A30/F8ZNCRP41/show_all_instances_sh.sh"
-            },
-            {
-                "type": "button",
-                "text": ":broom: Clean up my stuff",
-                "url": "https://console.aws.amazon.com/ec2/v2/home"
-            },
-        ]
-    )
+    # send_slack_message(
+    #     msg_text, 
+    #     title='AWS Instance Reaper Report :reaper:',
+    #     text="```\n"+lb+"\n```",
+    #     fallback='AWS Instance Reaper Report :reaper:',
+    #     color='warning'
+    # )
     
 def send_slack_message(msg_text, **kwargs):
     """Sends a slack message to the slackChannel you specify. The only parameter
@@ -76,31 +64,35 @@ def send_slack_message(msg_text, **kwargs):
     except URLError as e:
         logger.error("Server connection failed: %s", e.reason)
         
-def get_untagged_instances():
+def get_tagged_instances():
     """Calls the Lambda function that returns a dictionary of instances."""
     try:
-        response = lam.invoke(FunctionName='getUntaggedInstances', InvocationType='RequestResponse')
+        response = lam.invoke(FunctionName='getTaggedInstances', InvocationType='RequestResponse')
     except Exception as e:
         print(e)
         raise e
     return response
     
-def generate_leaderboard(response,num_leaders):
-    """Generates a leaderboard showing KeyNames with the most untagged instances."""
+def generate_expired_dict(response):
+    """Generates a dictionary of instances that have passed their Time to Live (TTL)."""
     data = json.loads(response['Payload'].read().decode('utf-8'))
     data = json.loads(data)
-    sshkeys = []
+    expired_instances = {}
     for key, value in data.items():
-        sshkeys.append(value['KeyName'])
-    leaders = dict(Counter(sshkeys))
-    tmp = io.StringIO()
-    writer = csv.writer(tmp, delimiter='\t')
-    count=0
-    # This is a fancy way to say 'return leaders in reverse numerical order'.
-    for key, value in sorted(leaders.items(), key=lambda x: x[1], reverse=True):
-        if count < num_leaders:
-            writer.writerow(["{: >2}".format(value), (key or 'No KeyName')])
-        count = count + 1
-    leaderboard = tmp.getvalue()
-    # To keep things simple we make sure these functions always return a string.
-    return(leaderboard)
+        expired_instances[key] = expired_instances[value['TTL']]
+    logger.info(expired_instances)
+    return expired_instances
+
+# This could be useful for generating email reports or dumping a list of untagged
+# instances into an S3 bucket.
+def generate_tsv(response):
+    """Ingests data from a lambda response, converts it to tab-separated format."""
+    data=json.loads(response['Payload'].read().decode('utf-8'))
+    data=json.loads(data)
+    output = io.StringIO()
+    writer = csv.writer(output, delimiter='\t')
+    for key, value in data.items():
+        value['InstanceId'] = key
+        writer.writerow(value.values())
+    contents = output.getvalue()
+    return(contents)
