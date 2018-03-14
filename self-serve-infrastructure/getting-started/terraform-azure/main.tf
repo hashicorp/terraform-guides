f74e5a48fe47eeaf5c4371a3460d50fc09ca8d52
Fraser Pollock <fraserpol@gmail.com>
2018-03-14 09:46:35 -0400

    Minor tweaks to starting modules for getting started demo

--- self-serve-infrastructure/getting-started/terraform-azure/main.tf
+++ self-serve-infrastructure/getting-started/terraform-azure/main.tf
@@ -1,20 +1,25 @@
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = "${var.name}"
-  location            = "${var.network_location}"
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["${var.name}-1", "${var.name}-2", "${var.name}-3"]
-
-  tags = "${merge(var.tags, map("Name", format("%s-%d", var.name, count.index+1)))}"
+provider "azurerm" {
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
 
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
-  location            = "${var.compute_location}"
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["${var.name}"] // change to a unique name per datacenter region
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
+resource "azurerm_resource_group" "demo_resource_group" {
+  name     = "${var.rg_name}"
+  location = "${var.rg_location}"
+}
 
-  tags = "${merge(var.tags, map("Name", format("%s-%d", var.name, count.index+1)))}"
+resource "azurerm_virtual_network" "demo_virtual_network" {
+  name                = "${var.vn_name}"
+  address_space       = ["${var.vn_address_space}"]
+  location            = "${azurerm_resource_group.demo_resource_group.location}"
+  resource_group_name = "${azurerm_resource_group.demo_resource_group.name}"
+}
+
+resource "azurerm_subnet" "demo_subnet" {
+  name                 = "${var.sb_name}"
+  resource_group_name  = "${azurerm_resource_group.demo_virtual_network.name}"
+  virtual_network_name = "${azurerm_virtual_network.demo_virtual_network.name}"
+  address_prefix       = "${var.sb_address_prefix}"
 }
