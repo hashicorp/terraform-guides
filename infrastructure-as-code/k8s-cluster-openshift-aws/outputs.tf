38422c3d57e159f0c92ab2409a7aa29fd61bb7f6
Roger Berlind <roger@hashicorp.com>
2018-05-01 19:37:47 -0400

    removed vault_reviewer_token output

--- infrastructure-as-code/k8s-cluster-openshift-aws/outputs.tf
+++ infrastructure-as-code/k8s-cluster-openshift-aws/outputs.tf
@@ -31,10 +31,6 @@ output "k8s_master_auth_cluster_ca_certificate" {
   value = "${data.null_data_source.get_certs.outputs["ca_certificate"]}"
 }
 
-output "vault_reviewer_token" {
-  value = "${data.null_data_source.get_certs.outputs["vault_reviewer_token"]}"
-}
-
 output "vault_k8s_auth_backend" {
   value = "${vault_auth_backend.k8s.path}"
 }
