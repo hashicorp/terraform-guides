a6f8a1c55fbc6ee02bd1b60f97b78282120512d8
Dave Arnold <darnold@hashicorp.com>
2018-06-15 15:36:46 -0700

    fixing this to use ami lookup so we dont use expired AMIs and setting az explicitly as it was causing failures

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -6,10 +6,26 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
+data "aws_ami" "ubuntu_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+  ami               = "${data.aws_ami.ubuntu_ami.id}"
+  instance_type     = "${var.instance_type}"
+  availability_zone = "${var.aws_availability_zone}"
 
   tags {
     Name = "${var.name}"
--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -1,19 +1,24 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default     = "us-west-1"
+}
+
+variable "aws_availability_zone" {
+  description = "AWS Availability Zone"
+  default     = "us-west-1b"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default     = "ami-2e1ef954"
 }
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "Provisioned by Terraform"
+  default     = "Provisioned by Terraform"
 }
