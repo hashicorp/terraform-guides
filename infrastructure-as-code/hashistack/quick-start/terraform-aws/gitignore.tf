f960107d802ea125424e897541cc36f78caf8359
Jon Benson <bensojon@gmail.com>
2018-07-21 21:24:16 -0700

    Update gitignore

---
+++ infrastructure-as-code/hashistack/quick-start/terraform-aws/gitignore.tf
@@ -0,0 +1,7 @@
+# `.tf` files that contain the word "gitignore" are ignored
+# by git in the `.gitignore` file at the root of this repo.
+
+# If you have local Terraform configuration that you want
+# ignored like Terraform backend configuration, create
+# a new file (separate from this one) that contains the
+# word "gitignore" (e.g. `backend.gitignore.tf`).
