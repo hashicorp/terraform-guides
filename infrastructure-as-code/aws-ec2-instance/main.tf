8a42c80ea73c7feb7ed3f24609a2d1c8f069cfc3
Beau Labs <46016124+beaulabs@users.noreply.github.com>
2019-04-02 12:49:23 -0600

    updated ami reference code

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -7,11 +7,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
 
   tags {
     Name = "${var.name}"
   }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
 }
