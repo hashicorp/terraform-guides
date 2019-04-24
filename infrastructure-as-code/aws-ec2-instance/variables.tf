3b2accdcd7eec54bee78df19486ff54a55d6009d
Beau Labs <46016124+beaulabs@users.noreply.github.com>
2019-04-24 13:25:52 -0600

    added tags

--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -1,19 +1,23 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-2"
+  default     = "us-west-2"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default     = "ami-2e1ef954"
 }
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.micro"
+  default     = "t2.micro"
+
+  tags = {
+    environment = "development"
+  }
 }
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "Provisioned by Terraform Ent"
+  default     = "Provisioned by Terraform Ent"
 }
