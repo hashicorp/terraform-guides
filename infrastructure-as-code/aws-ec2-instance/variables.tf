ea665c07b8a0563b4b2f0b3315624c7052d5a520
JohnnyB <jboero@users.noreply.github.com>
2019-06-10 03:14:58 +0100

    Updated AMI

--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-041b16972dd1462d4"
+  default = "ami-afa31dd7"
 }
 
 variable "instance_type" {
