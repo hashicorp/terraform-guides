93202c578fcfba997fd43a92fec5a5da5e24b0b4
JohnnyB <jboero@users.noreply.github.com>
2019-06-10 02:54:10 +0100

    Update variables for Tokyo workshop
    Switching to ap-northeast-1 and AMI Amazon Linux

--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "ap-northeast-1"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-0f889fe6d4bc85550"
 }
 
 variable "instance_type" {
