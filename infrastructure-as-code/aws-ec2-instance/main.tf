7d0685f5eb707986d7cf2135baaa9885c29787ac
MertSenel <32119844+MertSenel@users.noreply.github.com>
2019-09-02 10:41:23 +1000

    Update main.tf
    fixed typo

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+  availability_zone = "${var.aws_region}"
 
   tags {
     Name = "${var.name}"
