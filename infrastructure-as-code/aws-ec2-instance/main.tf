979e8dc541cce012fefa914da72fdc2408249f0b
Sébastien Braun <sebbraun@gmail.com>
2019-09-09 18:59:15 +0200

    updated for terraform 0.12

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -1,17 +1,33 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.12"
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
+  ami               = "${data.aws_ami.ubuntu.id}"
+  instance_type     = var.instance_type
   availability_zone = "${var.aws_region}a"
 
-  tags {
-    Name = "${var.name}"
+  tags = {
+    Name = var.name
   }
 }
--- infrastructure-as-code/aws-ec2-instance/outputs.tf
+++ infrastructure-as-code/aws-ec2-instance/outputs.tf
@@ -1,3 +1,4 @@
 output "public_dns" {
-  value = "${aws_instance.ubuntu.public_dns}"
+  value = aws_instance.ubuntu.public_dns
 }
+
