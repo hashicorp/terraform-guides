79c183f76f786ab00bc2d1a6a995dc20295de6e8
JohnnyB <jboero@users.noreply.github.com>
2019-12-02 10:48:12 +0000

    Updated for TF 0.12

--- infrastructure-as-code/gcp-compute-instance/main.tf
+++ infrastructure-as-code/gcp-compute-instance/main.tf
@@ -63,5 +63,5 @@ resource "google_compute_instance" "demo" {
 }
 
 output "external_ip"{
-  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${google_compute_instance.demo.network_interface.0.access_config.0.nat_ip}"
 }
